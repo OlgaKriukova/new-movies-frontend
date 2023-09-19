ef7b84eae4252bf8b1f03bc65dce7acedd625b71
Iaroslav Kriukov <kryukovyar@yandex-team.ru>
2023-09-20 00:02:49 +0300

    generalForm

--- src/components/GeneralForm/GeneralForm.js
+++ src/components/GeneralForm/GeneralForm.js
@@ -2,40 +2,46 @@ import { Link } from 'react-router-dom';
 
 import './GeneralForm.css'
 
-function GeneralForm() {
+function GeneralForm(props) {
     return(
         <div className="form profile-form">
                 <h1 className="title-form">Добро пожаловать!</h1>
                 <form className="form__table">
-                    <div className="form__list form__list_active profile-form__wrap">
-                        <label htmlFor="name" className="form__label profile-form__label">
-                            Имя
-                        </label>
-                        <input
-                            id="name"
-                            className="form__input"
-                            type="text"
-                            placeholder="Виталий"
-                            required=""
-                            minLength={2}
-                            maxLength={25}
-                            defaultValue=""
-                        />
-                    </div>
-                    <div className="form__list form__list_active">
-                        <label htmlFor="email" className="form__label profile-form__label">
-                            E-mail
-                        </label>
-                        <input
-                            id="name"
-                            className="form__input"
-                            type="email"
-                            placeholder="pochta@yandex.ru"
-                            required=""
-                            minLength={7}
-                            maxLength={25}
-                        />
-                    </div>
+                    {props.existingInputs.includes('name')
+                      ? <div className="form__list form__list_active profile-form__wrap">
+                            <label htmlFor="name" className="form__label profile-form__label">
+                                Имя
+                            </label>
+                            <input
+                                id="name"
+                                className="form__input"
+                                type="text"
+                                placeholder="Виталий"
+                                required=""
+                                minLength={2}
+                                maxLength={25}
+                                defaultValue=""
+                            />
+                        </div>
+                      : <></>
+                    }
+                    {props.isEmailExist
+                      ? <div className="form__list form__list_active">
+                            <label htmlFor="email" className="form__label profile-form__label">
+                                E-mail
+                            </label>
+                            <input
+                                id="name"
+                                className="form__input"
+                                type="email"
+                                placeholder="pochta@yandex.ru"
+                                required=""
+                                minLength={7}
+                                maxLength={25}
+                            />
+                        </div>
+                        : <></>
+                    }
                     <div className="form__list form__list_active">
                         <label htmlFor="password" className="form__label">
                             Пароль
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,68 +1,15 @@
 import { Link } from 'react-router-dom';
+import GeneralForm from '../GeneralForm/GeneralForm';
 
 import './Register.css';
 
 function Register() {
     return(
-        <>
-            <div className="form">
-                <h1 className="title-form">Добро пожаловать!</h1>
-                <form className="form__table">
-                    <div className="form__list">
-                        <label htmlFor="name" className="form__label">
-                            Имя
-                        </label>
-                        <input
-                            id="name"
-                            className="form__input"
-                            type="text"
-                            placeholder="Виталий"
-                            required=""
-                            minLength={2}
-                            maxLength={25}
-                            defaultValue=""
-                        />
-                    </div>
-                    <div className="form__list">
-                        <label htmlFor="email" className="form__label">
-                            E-mail
-                        </label>
-                        <input
-                            id="name"
-                            className="form__input"
-                            type="email"
-                            placeholder="pochta@yandex.ru"
-                            required=""
-                            minLength={7}
-                            maxLength={25}
-                        />
-                    </div>
-                    <div className="form__list">
-                        <label htmlFor="password" className="form__label">
-                            Пароль
-                        </label>
-                        <input
-                            id="password"
-                            className="form__input"
-                            type="password"
-                            placeholder="••••••••••••••"
-                            required=""
-                        />
-                        <span className="form__input-error">Что-то пошло не так...</span>
-                    </div>
-                    <div className="reg">
-                    <button className="reg__button">Зарегистрироваться</button>
-                    <div className="reg__enter">
-                        <p className="reg__enter-text">Уже зарегистрированы?</p>
-                        <Link to="/signin" className="reg__enter-link">
-                        Войти
-                        </Link>
-                    </div>
-                    </div>
-                </form>
-            </div>
-
-        </>
+        <GeneralForm
+          existingInputs ={['name','email','password']}
+          isNameExist = {true}
+          isEmailExist = {true}
+        />
     );
 }
 
